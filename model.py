0b4b92041838c6b721b6b59c116615a247008cdb
Pedro Ferracini <pedro.ferracini@imusics.com>
2019-11-05 12:08:08 -0300

    Revert "Implementa divisão de dimensão de embeddings"
    This reverts commit 6d340321c655776d706a187c0ea79ae15bbcc69d.

--- model.py
+++ model.py
@@ -129,8 +129,6 @@ class Lstm(torch.nn.Module):
         self.emb_e = torch.nn.Embedding(num_entities, args.embedding_dim, padding_idx=0)
         self.emb_rel = torch.nn.Embedding(num_relations, args.embedding_dim, padding_idx=0)
         self.loss = torch.nn.BCELoss()
-        self.emb_dim1 = args.embedding_shape1
-        self.emb_dim2 = args.embedding_dim // self.emb_dim1
         self.batch_size = args.batch_size
         self.timesteps = args.timesteps
         self.embedding_dim = args.embedding_dim
@@ -138,8 +136,8 @@ class Lstm(torch.nn.Module):
         self.emb_dim1 = args.embedding_shape1
         self.emb_dim2 = args.embedding_dim // self.emb_dim1
 
-        self.rnn1 = torch.nn.LSTM(input_size=20, hidden_size=args.hidden_size//2, num_layers=args.num_layers, batch_first=True)
-        self.rnn2 = torch.nn.LSTM(input_size=20, hidden_size=args.hidden_size//2, num_layers=args.num_layers, batch_first=True)
+        self.rnn1 = torch.nn.LSTM(input_size=self.embedding_dim//self.timesteps * args.num_layers, hidden_size=args.hidden_size//2, num_layers=args.num_layers, batch_first=True)
+        self.rnn2 = torch.nn.LSTM(input_size=self.embedding_dim//self.timesteps * args.num_layers, hidden_size=args.hidden_size//2, num_layers=args.num_layers, batch_first=True)
         self.fc = torch.nn.Linear(args.hidden_size,args.embedding_dim)
         self.register_parameter('b', Parameter(torch.zeros(num_entities)))
 
@@ -150,23 +148,19 @@ class Lstm(torch.nn.Module):
         xavier_normal_(self.emb_rel.weight.data)
         
     def forward(self, e1, rel):
-        e1_embedded= self.emb_e(e1).view(-1, 1, self.emb_dim1, self.emb_dim2)
-        rel_embedded = self.emb_rel(rel).view(-1, 1, self.emb_dim1, self.emb_dim2)
+        e1_embedded= self.emb_e(e1)
+        rel_embedded = self.emb_rel(rel)
         # stacked_inputs = torch.cat([e1_embedded, rel_embedded], 2)
 
         # print("stacked")
         # print(stacked_inputs.shape)
 
-        x1 = e1_embedded.view(self.batch_size, 10, -1)
-        x2 = rel_embedded.view(self.batch_size, 10, -1)
+        x1 = e1_embedded.view(self.batch_size, self.timesteps, -1)
+        x2 = rel_embedded.view(self.batch_size, self.timesteps, -1)
 
-        # print("x1 pos view")
-        # print(x1)
-        # print(x1.shape)
-
-        # print("x2 pos view")
-        # print(x2)
-        # print(x2.shape)
+        # print("stacked pos view")
+        # print(x)
+        # print(x.shape)
 
         x1, (hn1, cn1) = self.rnn1(x1)
         x2, (hn2, cn2) = self.rnn2(x2)
